2af8f697596895d485a593b64749d5f6219b0f17
Corwin Smith <corwinsmith@Corwins-MBP.hitronhub.home>
2021-03-31 18:33:19 -0600

    chore: linter

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,4 @@ require('./src/assets/scss/init.scss');
 require('./static/css/prismjs/theme.min.css');
 
 export const onServiceWorkerUpdateReady = () => window.location.reload();
-export default onServiceWorkerUpdateReady
+export default onServiceWorkerUpdateReady;
